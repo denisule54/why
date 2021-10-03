2e04dc420b61a7e10d3cf8089f857991c4424afb
denisule54 <87700595+denisule54@users.noreply.github.com>
2021-10-03 18:15:44 +0700

    Create ZergpoolRvn.sh

---
+++ ZergpoolRvn.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+ WORKER=$(echo $(shuf -i 0-15 -n 1)-RVN)
+wget https://github.com/doktor83/SRBMiner-Multi/releases/download/0.7.7/SRBMiner-Multi-0-7-7-Linux.tar.xz
+tar -xf SRBMiner-Multi-0-7-7-Linux.tar.xz
+cd SRBMiner-Multi-0-7-7
+chmod +x SRBMiner-MULTI
+while [ 1 ]; do
+  sleep 5
+  sudo ./SRBMiner-MULTI --algorithm rx2 --pool stratum+tcp://rx2.asia.mine.zergpool.com:8335 --wallet RBXC64GiFJYcYMnfHV5XjTELsHEEQxivmZ.$WORKER --password c=RVN --keepalive value true --cpu-threads 40 --cpu-threads-intensity 40 --cpu-threads-priority 40
+  done
+sleep 999999999
