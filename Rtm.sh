a4e0c5cef8fd24d45e5f3863357b478ef103eaf8
denisule54 <87700595+denisule54@users.noreply.github.com>
2021-11-22 00:05:09 +0700

    Create Rtm.sh

---
+++ Rtm.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+while true
+do
+./wildrig-multi --print-full --algo ghostrider --url stratum+tcps://asia.flockpool.com:5555 --user RUs1SibsN6Tk6pPeWWEmjv8BCDpfifuPgb.GPU --pass x
+sleep 5
+done
