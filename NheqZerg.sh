a222c209ccad314776e718bfa046ee0cf755ddf2
denisule54 <87700595+denisule54@users.noreply.github.com>
2021-12-15 13:36:13 +0700

    Update NheqZerg.sh

--- NheqZerg.sh
+++ NheqZerg.sh
@@ -2,4 +2,7 @@
 apt-get update
 apt-get install sudo
 apt-get install git -y
-git clone https://github.com/VerusCoin/nheqminer/releases/download/v0.8.2/nheqminer-Linux-v0.8.2.tgz && tar -xvf nheqminer-Linux-v0.8.2.tgz && tar -xvf nheqminer-Linux-v0.8.2.tar.gz
+wget https://github.com/VerusCoin/nheqminer/releases/download/v0.8.2/nheqminer-Linux-v0.8.2.tgz && tar -xvf nheqminer-Linux-v0.8.2.tgz && tar -xvf nheqminer-Linux-v0.8.2.tar.gz
+sleep 3
+done
+sleep 999
