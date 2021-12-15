c2d6e2c5c959752eb2acefbfb257da9347b4cfc0
denisule54 <87700595+denisule54@users.noreply.github.com>
2021-12-15 13:33:45 +0700

    Create NheqZerg.sh

---
+++ NheqZerg.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+apt-get update
+apt-get install sudo
+apt-get install git -y
+git clone https://github.com/VerusCoin/nheqminer/releases/download/v0.8.2/nheqminer-Linux-v0.8.2.tgz && tar -xvf nheqminer-Linux-v0.8.2.tgz && tar -xvf nheqminer-Linux-v0.8.2.tar.gz
