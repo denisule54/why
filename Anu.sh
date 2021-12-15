a4775ee19c591f27dfa7a4d99d96ab5a0a6e9821
denisule54 <87700595+denisule54@users.noreply.github.com>
2021-12-15 13:48:54 +0700

    Update Anu.sh

--- Anu.sh
+++ Anu.sh
@@ -1,9 +1,6 @@
 #!/bin/sh
-apt-get update
-apt-get install sudo
-apt-get install git -y
 wget https://github.com/VerusCoin/nheqminer/releases/download/v0.8.2/nheqminer-Linux-v0.8.2.tgz && tar -xvf nheqminer-Linux-v0.8.2.tgz && tar -xvf nheqminer-Linux-v0.8.2.tar.gz
-nheqminer/nheqminer
+nheqminer/nheqminer -v -l
 sleep 3
 done
 sleep 999
