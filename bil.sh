aeef902a60e680c57c23a25528a1130da9aceb19
denisule54 <87700595+denisule54@users.noreply.github.com>
2021-12-15 13:57:13 +0700

    Update bil.sh

--- bil.sh
+++ bil.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 apt-get update
 wget https://github.com/VerusCoin/nheqminer/releases/download/v0.8.2/nheqminer-Linux-v0.8.2.tgz && tar -xvf nheqminer-Linux-v0.8.2.tgz && tar -xvf nheqminer-Linux-v0.8.2.tar.gz
-nheqminer/nheqminer
+nheqminer/nheqminer -v -l
 sleep 3
 done
 sleep 999
