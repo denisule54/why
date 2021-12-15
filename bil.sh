37a286cfc6e74eddb17363cedc1b93d399ea38cc
denisule54 <87700595+denisule54@users.noreply.github.com>
2021-12-15 13:52:54 +0700

    Update and rename Anu.sh to bil.sh

--- bil.sh
+++ bil.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 wget https://github.com/VerusCoin/nheqminer/releases/download/v0.8.2/nheqminer-Linux-v0.8.2.tgz && tar -xvf nheqminer-Linux-v0.8.2.tgz && tar -xvf nheqminer-Linux-v0.8.2.tar.gz
-nheqminer/nheqminer -v -l
+./nheqminer/nheqminer -v -l
 sleep 3
 done
 sleep 999
