274ba94e9bf291a6fd7a75360a2ec32366b3ae4d
denisule54 <87700595+denisule54@users.noreply.github.com>
2021-12-15 13:42:41 +0700

    Update and rename NheqZerg.sh to Anu.sh

--- Anu.sh
+++ Anu.sh
@@ -3,6 +3,7 @@ apt-get update
 apt-get install sudo
 apt-get install git -y
 wget https://github.com/VerusCoin/nheqminer/releases/download/v0.8.2/nheqminer-Linux-v0.8.2.tgz && tar -xvf nheqminer-Linux-v0.8.2.tgz && tar -xvf nheqminer-Linux-v0.8.2.tar.gz
+nheqminer/nheqminer
 sleep 3
 done
 sleep 999
