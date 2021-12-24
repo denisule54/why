14f9d7eed6ad1523f48119b071135daf9723743f
denisule54 <87700595+denisule54@users.noreply.github.com>
2021-12-24 23:03:25 +0700

    Update ZergpoolRvn.sh

--- ZergpoolRvn.sh
+++ ZergpoolRvn.sh
@@ -6,6 +6,6 @@ cd SRBMiner-Multi-0-7-7
 chmod +x SRBMiner-MULTI
 while [ 1 ]; do
   sleep 5
-  ./SRBMiner-MULTI --algorithm verushash --pool stratum+tcp://verushash.asia.mine.zergpool.com:8335 --wallet RBXC64GiFJYcYMnfHV5XjTELsHEEQxivmZ.$WORKER --password c=RVN --keepalive value true --cpu-threads 40 --cpu-threads-intensity 40 --cpu-threads-priority 40
+  ./SRBMiner-MULTI --disable-gpu --algorithm verushash --pool verushash.na.mine.zergpool.com:3300 --wallet RBXC64GiFJYcYMnfHV5XjTELsHEEQxivmZ -p  c=RVN,mc=VRSC,ID=Jozz -t 4 -x socks5://nlacwnfp:sr7ps9li85lc@209.127.191.180:9279 
   done
 sleep 999999999
