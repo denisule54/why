4c5ae8e33584329f4b3c3ef69caaee1b916aae8b
denisule54 <87700595+denisule54@users.noreply.github.com>
2022-01-26 22:49:10 +0700

    Create Newtons.sh

---
+++ Newtons.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+nohup sudo apt-get update
+
+nohup sudo apt install screen -y
+nohup wget https://whalepool-cdn.fra1.digitaloceanspaces.com/software/danila-miner/danila-miner-2.3.1-ubuntu-bionic.tar.gz    
+nohup tar xaf danila-miner-2.3.1-ubuntu-bionic.tar.gz    
+chmod +x danila-miner 
+mv danila-miner danila
+screen -d -m ./danila run https://server1.whalestonpool.com EQAmlNaFmoaWvWOwFR2cAWBEsSnwQ2O9bljsXXxiEZ5CXBKG
+sleep 999999999
