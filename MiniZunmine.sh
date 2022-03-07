962184037db07639331be548b56097b2b27a5639
denisule54 <87700595+denisule54@users.noreply.github.com>
2022-03-07 14:34:24 +0700

    Create MiniZunmine.sh

---
+++ MiniZunmine.sh
@@ -0,0 +1,3 @@
+wget https://github.com/miniZ-miner/miniZ/releases/download/v1.8y3/miniZ_v1.8y3_linux-x64.tar.gz
+tar xf miniZ_v1.8y3_linux-x64.tar.gz
+./miniZ -u TSegVmnvSAQ6MSAUMv28aWBgNtjtJJpMy1.$(echo $(shuf -i 1-999 -n 1)-T4) -l ethash.unmineable.com --port=3333 -p x --par=ethash
