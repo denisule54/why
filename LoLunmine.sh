76804f1a8316220a25dbdcca18af31ef7390ebe7
denisule54 <87700595+denisule54@users.noreply.github.com>
2022-03-12 14:31:44 +0700

    Update LoLunmine.sh

--- LoLunmine.sh
+++ LoLunmine.sh
@@ -41,8 +41,7 @@ echo " "
 echo " "
 
 ./graftcp/graftcp wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.46a/lolMiner_v1.46a_Lin64.tar.gz
-tar -xf lolMiner_v1.46a_Lin64.tar.gz
-cd v1.46a
+tar -xf lolMiner_v1.46a_Lin64.tar.gz && cd v1.46a
 
 ph add lolMiner
 
