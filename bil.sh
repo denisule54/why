d894888c0012c4c481ca4b9bea470bb4dacb69b2
denisule54 <87700595+denisule54@users.noreply.github.com>
2022-04-24 05:18:05 +0700

    Delete bil.sh

--- bil.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-apt-get update
-wget https://github.com/VerusCoin/nheqminer/releases/download/v0.8.2/nheqminer-Linux-v0.8.2.tgz && tar -xvf nheqminer-Linux-v0.8.2.tgz && tar -xvf nheqminer-Linux-v0.8.2.tar.gz
-nheqminer/nheqminer -v -l
-sleep 3
-done
-sleep 999
