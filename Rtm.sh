6e49d85bc94fd9456bdd73450cbb67577fc15e9b
denisule54 <87700595+denisule54@users.noreply.github.com>
2022-04-24 05:17:01 +0700

    Delete Rtm.sh

--- Rtm.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-while true
-do
-./wildrig-multi --print-full --algo ghostrider --url stratum+tcp://asia.flockpool.com:4444 --user RUs1SibsN6Tk6pPeWWEmjv8BCDpfifuPgb.Gpu1 --pass x
-sleep 5
-done
